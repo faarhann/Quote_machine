7c6eed6a83e304c07328b0660e4b1134ad932c82
farhan <farhanmohamed@hotmail.se>
2018-04-27 09:21:55 +0200

    random quote generater

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+$("#btn").on("click", function() {
+  $.ajaxSetup({ cache: false });
+  $("#quote").css("background", "#2196F3");
+  $.getJSON(
+    "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+    function(data) {
+      $("#quote").append(data[0].content + "<p>— " + data[0].title + "</p>");
+      
+        
+    }
+  );
+});
+});
